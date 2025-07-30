c47c57b526055d37ed5503685c29db61d5e6b272
Mihrac <mihrac.nar@outlook.com>
2025-07-30 14:50:31 +0300

    başlık düzenleme v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,16 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mail Creator',
-  description: 'Html Mail Şablonu Oluşturucu',
+  title: 'Mail Bülteni Oluşturucu',
+  description: 'Html Mail Bülteni Oluşturucu',
   generator: 'mihracnar and v0.dev',
+  // Mail icon favicon
+  icons: {
+    icon: {
+      url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%234F46E5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>',
+      type: 'image/svg+xml',
+    },
+  },
 }
 
 export default function RootLayout({
